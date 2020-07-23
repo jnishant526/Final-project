9ea7f46ef50f8667ed94e41016fc6a5f82d1d923
Nishan joshi <67043499+jnishant526@users.noreply.github.com>
2020-07-23 16:09:39 +0530

    Add files via upload

---
+++ js/load.js
@@ -0,0 +1,8 @@
+var typed = new Typed('.typing', {
+    strings: [" Technocolabs is non-profit organization which is followed by technical experts for enthusiastic learners","Artificial intelligence machine learning have great way to develop.","Technical Colabs is a leading tech Startups with learn something new and innovative ideas..."],
+    loop: true,
+    typeSpeed: 15,
+    backSpeed: 40,
+    backDelay:500
+  });
+
